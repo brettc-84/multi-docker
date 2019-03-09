3349721d19331d6c3089b174f08689fafe28e296
brettc-84 <brett.coomer@gmail.com>
2019-03-09 16:02:03 +0200

    values/all endpoint

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const redisPublisher = redisClient.duplicate();
 app.get('/', (req, res) => {
   res.send('Hi');
 });
-app.get('/all', async (req, res) => {
+app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
   res.send(values.rows);
 });
